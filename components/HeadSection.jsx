93e15cece8c6e65970d6bde2b622b4cfc61a901d
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-04 05:51:22 +0300

    DataProducts_TO_Layout

---
+++ components/HeadSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavSection from "./NavSection";
+import Header from "./Header";
+
+const HeadSection = () => {
+  return (
+    <div className=" bg-gray-500">
+      <NavSection />
+      <Header />
+    </div>
+  );
+};
+
+export default HeadSection;
---
+++ components/NavSection.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+
+const NavSection = () => {
+  return (
+    <nav className="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
+      <div className="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
+        <div className="flex items-center gap-2">
+          <Image
+            width={50}
+            height={50}
+            alt="Menu icon"
+            src="/assets/svg/menu.svg"
+            className="lg:hidden w-5 h-5"
+          />
+
+          <a href="home">
+            <Image
+              width={100}
+              height={100}
+              alt="Logo"
+              src="/assets/lws-logo-black.svg"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-2 lg:gap-5 lg:w-4/12">
+        <Image
+          width={100}
+          height={100}
+          src="/assets/svg/avatar.svg"
+          className="hidden lg:block w-[18px] h-[18px]"
+          alt="Login account avatar"
+        />
+        <Image
+          width={100}
+          height={100}
+          src="/assets/svg/shopping-Cart.svg"
+          className="block w-5 h-5"
+          alt="Shopping cart icon"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default NavSection;
